14c0e0eea5213143cac14c64ec05e16eae33660d
Felipe Rodolfo <felipewebdeveloper>
2023-10-20 00:03:03 -0300

    code refactoring

---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot/src/main/java/com/felipe/demo/math/SimpleMath.java
@@ -0,0 +1,27 @@
+package com.felipe.demo.math;
+
+public class SimpleMath {
+	
+	public Double sum(Double numberOne,Double numberTwo)
+	{
+		return numberOne + numberTwo;
+	}
+	
+	public Double subtraction(Double numberOne, Double numberTwo){
+
+		return numberOne - numberTwo;
+	}
+	
+	public Double multiplication(Double numberOne, Double numberTwo)  {
+		return numberOne * numberTwo;
+	}
+	
+	public Double division(Double numberOne,Double numberTwo)  {
+		return numberOne / numberTwo;
+	}
+	
+	public Double mean(Double numberOne,Double numberTwo) {
+		return (numberOne + numberTwo) / 2;
+	}
+	
+}
